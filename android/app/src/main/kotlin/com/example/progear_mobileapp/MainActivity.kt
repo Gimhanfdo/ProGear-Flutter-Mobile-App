bef99b0609142f87697b217f813afe56412c3699
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-10-06 16:19:05 +0530

    updated MainActivity.kt

--- android/app/src/main/kotlin/com/example/progear_mobileapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/progear_mobileapp/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.progear_mobileapp
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
